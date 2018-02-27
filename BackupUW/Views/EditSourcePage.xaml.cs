ed6ba84f132414d92d436c575004d3955b7dd00b
Robert McComb <mccombenator@gmail.com>
2018-02-27 17:23:30 +1000

    New views and basic nav

---
+++ BackupUW/Views/EditSourcePage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BackupUW.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EditSourcePage : Page
+    {
+        public EditSourcePage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Debug.WriteLine("EditSourcePage.OnNavigatedTo");
+
+            if (e.Parameter == null)
+            {
+                // Adding new item
+
+            }
+
+            if (e.Parameter is string)
+            {
+                Debug.WriteLine("Clicked on {0}", e.Parameter);
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void PickDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            FolderPicker folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add(".docx");
+            folderPicker.FileTypeFilter.Add(".xlsx");
+            folderPicker.FileTypeFilter.Add(".pptx");
+            StorageFolder folder = folderPicker.PickSingleFolderAsync().GetResults();
+            if (folder != null)
+            {
+                // Application now has read/write access to all contents in the picked folder (including other sub-folder contents)
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                Directory.Text = folder.Name;
+            }
+            else
+            {
+                //OutputTextBlock.Text = "Operation cancelled.";
+            }
+        }
+
+        //StorageFolder picturesFolder = await KnownFolders.GetFolderForUserAsync(null /* current user */, KnownFolderId.PicturesLibrary);
+
+        //IReadOnlyList<StorageFile> fileList = await picturesFolder.GetFilesAsync();
+        //IReadOnlyList<StorageFolder> folderList = await picturesFolder.GetFoldersAsync();
+
+
+
+    }
+}
